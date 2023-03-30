04145f3a2da0947d7c4d40f808bb5557906f7f99
agustintrossero <agustintrossero@gmail.com>
2023-03-30 20:41:17 +0200

    toda la logica necesaria para poder consumir el detalle de cada guitarra con routing dinamico

--- app/models/guitarras.server.js
+++ app/models/guitarras.server.js
@@ -1,6 +1,13 @@
+
+
 export async function getGuitarras(){
     const respuesta = await fetch(`${process.env.API_URL}/api/guitarras?populate=imagen`)
     return await respuesta.json()
     
     //console.log(process.env.API_URL)
   }
+
+export async function getGuitarra(url){
+  const respuesta = await fetch(`${process.env.API_URL}/api/guitarras?filters[url]=${url}&populate=imagen`)
+  return await respuesta.json()
+}
