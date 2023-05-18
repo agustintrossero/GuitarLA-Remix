27d97bb9161156a8a72a4d92d5f7a262de79f1c6
Agustin Trossero <96441753+agustintrossero@users.noreply.github.com>
2023-05-18 12:32:24 +0200

    pasamos toda la informacion de la guitarra agregada al carrito y la imprimimos con un map tambien se comprueba si el carrito esta vacio hace la validacion, toda la info la traemos con Contex y tambien se agrego estilos al carrito

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,3 +1,4 @@
+import { useOutletContext } from '@remix-run/react'
 import styles from '~/styles/carrito.css'
 
 export function links(){
@@ -17,12 +18,29 @@ export function meta(){
 }
 
 export default function Carrito() {
+    const { carrito } = useOutletContext();
+    //console.log(carrito)
   return (
     <main className="contenedor">
         <h1 className="heading">Carrito de Compras</h1>
         <div className="contenido">
             <div className="carrito">
                 <h2>Articulos</h2>
+                {carrito.length === 0 ? `Carrito Vacio` : (
+                    carrito.map( producto => (
+                        <div key={producto.id} className='producto'>
+                            <div>
+                                <img src={producto.imagen} alt={`Producto - Guitarra tipo ${producto.nombre}`}/>
+                            </div>
+                            <div>
+                                <p className='nombre'>{producto.nombre}</p>
+                                <p className='cantidad'>{producto.cantidad}</p>
+                                <p className="precio">$ <span>{producto.precio}</span></p>
+                                <p className="subtotal">Subtotal: $ <span>{producto.cantidad * producto.precio}</span></p>
+                            </div>
+                        </div>
+                    ))
+                )}
             </div>
             <aside className="resumen">
                 <h3>Resumen del Pedido</h3>
